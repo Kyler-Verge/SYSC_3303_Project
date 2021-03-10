7e0e0fbcc8a9a0f92a22fa573d5fef391d013f24
Justice Ayela <justiceayela@cmail.carleton.ca>
2021-03-10 17:18:22 -0500

    Test Commit / Justice

--- src/Test_Join.java
+++ src/Test_Join.java
@@ -1,5 +1,6 @@
 public class Test_Join {
     public static void main(String[] args){
-        System.out.println("Kyler was here.")
+        System.out.println("Kyler was here.");
+        System.out.println("Justice was here LOOOOLLL");
     }
 }
